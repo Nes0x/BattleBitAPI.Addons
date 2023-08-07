c71253f5899dd0c024f3bff2686a0c2afb8da088
Nes0x <przemek882@onet.pl>
2023-08-07 12:49:52 +0200

    Add try-catch to eventhandling

--- BattleBitAPI.Addons.EventHandler/Events/EventHandlerActivatorService.cs
+++ BattleBitAPI.Addons.EventHandler/Events/EventHandlerActivatorService.cs
@@ -1,20 +1,34 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace BattleBitAPI.Addons.EventHandler.Events;
 
 public class EventHandlerActivatorService<TPlayer> : IHostedService where TPlayer : Player
 {
     private readonly IEnumerable<IEventHandler<TPlayer>> _handlers;
+    private readonly ILogger<EventHandlerActivatorService<TPlayer>> _logger;
 
-    public EventHandlerActivatorService(IEnumerable<IEventHandler<TPlayer>> handlers)
+    public EventHandlerActivatorService(IEnumerable<IEventHandler<TPlayer>> handlers, ILogger<EventHandlerActivatorService<TPlayer>> logger)
     {
         _handlers = handlers;
+        _logger = logger;
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
         foreach (var handler in _handlers)
-            handler.Subscribe();
+        {
+            try
+            {
+                handler.Subscribe();
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message, e);
+            }
+
+        }
         return Task.CompletedTask;
     }
 
